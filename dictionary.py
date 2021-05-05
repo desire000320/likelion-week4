7e7d12a213b79046f0d50c5bb564394d1fc7af69
정망빙 <desire_32@naver.com>
2021-05-05 20:44:56 +0900

    add new

---
+++ dictionary.py
@@ -0,0 +1,23 @@
+#실습1번
+money = int(input("현재 가진 돈의 액수는?"))
+if money >= 10000:
+    print("고기를 먹는다")
+elif money >= 5000:
+    print("국밥을 먹는다")
+else:
+    print("라면을 먹는다")
+
+
+#if 예제
+score = 75
+if score >= 90:
+    print("A입니다")
+elif score >= 80:
+    print("B입니다.")
+else:
+    print("F입니다")
+
+
+#for
+for i in range (1,6)
+    print()
